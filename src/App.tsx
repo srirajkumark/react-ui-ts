2c7093bbd10b28a6757e56baf54d4c3c60b158d2
Srirajkumar <ksrirajk@email.com>
2024-09-23 22:54:31 +0530

    BootStrap and Font-Awesome Setup

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import './App.css';
 function App() {
   return (
     <React.Fragment>
-      <h2 className='colors'>App Component</h2>
+      <h2 className='text-primary'>App Component</h2>
+      <button className='btn btn-primary'>Start</button>
+      <i className='fa fa-home fa-2x'></i>
     </React.Fragment>
   );
 }
