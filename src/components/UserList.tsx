2370064ee3232cd7b03214195f252bcb7ed3ca1e
SRIRAJKUMAR K <ksrirajk@gmail.com>
2024-10-08 13:39:36 +0530

    List Rendering Setup

---
+++ src/components/UserList.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface IState{}
+interface IProps{}
+
+let UserList:React.FC = () => {
+    return(
+        <React.Fragment>
+            <h3>UserList</h3>
+        </React.Fragment>
+    )
+};
+export default UserList;
